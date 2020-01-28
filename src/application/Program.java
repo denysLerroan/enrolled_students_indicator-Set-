9db26a6710eac8319087577f1e7ed9db1fc74d00
DenysFerreira <lerroandeco@gmail.com>
2020-01-28 00:13:16 -0300

    Project completed (with class User)

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,46 +4,30 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
+import entities.User;
+
 public class Program {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 
-		Set<Integer> a = new HashSet<>();
-		Set<Integer> b = new HashSet<>();
-		Set<Integer> c = new HashSet<>();
+		Set<User> set = new HashSet<>();
 
-		System.out.print("How many students for course A? ");
+		System.out.print("How many courses for this instructor? ");
 		int n = sc.nextInt();
-		for (int i = 0; i < n; i++) {
-			int number = sc.nextInt();
-			a.add(number);
+
+		for (int i = 1; i <= n; i++) {
+			System.out.print("How many students for course " + i + "? ");
+			int numberOfStudents = sc.nextInt();
+			for (int j = 0; j < numberOfStudents; j++) {
+				int userCode = sc.nextInt();
+				set.add(new User(userCode));
+			}
 		}
 
-		System.out.print("How many students for course B? ");
-		n = sc.nextInt();
-		for (int i = 0; i < n; i++) {
-			int number = sc.nextInt();
-			b.add(number);
-		}
-
-		System.out.print("How many students for course C? ");
-		n = sc.nextInt();
-		for (int i = 0; i < n; i++) {
-			int number = sc.nextInt();
-			c.add(number);
-		}
-
-		Set<Integer> total = new HashSet<>(a);
-		total.addAll(b);
-		total.addAll(c);
-
-		System.out.println("Total students: " + total.size());
-		System.out.println("--------");
+		System.out.println("Total students: " + set.size());
 
 		sc.close();
-
 	}
-
 }
---
+++ src/entities/User.java
@@ -0,0 +1,44 @@
+package entities;
+
+public class User {
+
+	private Integer userCode;
+
+	public User(Integer userCode) {
+		this.userCode = userCode;
+	}
+
+	public Integer getUserCode() {
+		return userCode;
+	}
+
+	public void setUserCode(Integer userCode) {
+		this.userCode = userCode;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((userCode == null) ? 0 : userCode.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (userCode == null) {
+			if (other.userCode != null)
+				return false;
+		} else if (!userCode.equals(other.userCode))
+			return false;
+		return true;
+	}
+
+}
